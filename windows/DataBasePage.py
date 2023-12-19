ed1f58d068d6d56ef98617b4a4372e4dfeba6a9a
Kovalev Ivan <kovalev@logema.org>
2023-12-19 21:45:14 +0300

    to_refactoring Изменил код ,чтобы потом отрефакторить его

--- windows/DataBasePage.py
+++ windows/DataBasePage.py
@@ -1,5 +1,3 @@
-from itertools import count
-
 from PySide6.QtGui import QCloseEvent
 from PySide6.QtWidgets import QMainWindow, QTableWidgetItem
 
@@ -8,12 +6,11 @@ from generated.ui_databasepage import Ui_DataBasePage
 from model.models import Triangle
 
 
-class DataBasePage(QMainWindow):
+class DataBasePageController(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_DataBasePage()
         self.ui.setupUi(self)
-        self.session = Session()
         self.getDataTable()
         self.ui.tableWidget.cellChanged.connect(self.dataChange)
         self.ui.tableWidget.itemDoubleClicked.connect(self.importData)
@@ -22,30 +19,36 @@ class DataBasePage(QMainWindow):
         self.show()
 
     def getDataTable(self):
-        triangles = self.session.query(Triangle).all()
+        session = Session()
+        triangles = session.query(Triangle).all()
         self.ui.tableWidget.setRowCount(len(triangles))
         self.ui.tableWidget.setColumnHidden(0, True)
         for row, triangle in enumerate(triangles):
             self.insertRow(triangle, row)
 
-    def insertRow(self, triangle: Triangle, row: int):
-        item_id = QTableWidgetItem(str(triangle.id))
-        item_first_side = QTableWidgetItem(str(triangle.first_side))
-        item_second_side = QTableWidgetItem(str(triangle.second_side))
-
-        self.ui.tableWidget.setItem(row, 0, item_id)
-        self.ui.tableWidget.setItem(row, 1, item_first_side)
-        self.ui.tableWidget.setItem(row, 2, item_second_side)
-
     def dataChange(self, row: int, col: int):
+        session = Session()
         item = self.ui.tableWidget.item(row, col)
         if item is not None:
             triangle_id = int(self.ui.tableWidget.item(row, 0).text())
             if col == 1:
-                self.session.query(Triangle).filter_by(id=triangle_id).update({Triangle.first_side: float(item.text())})
+                session.query(Triangle).filter_by(id=triangle_id).update({Triangle.first_side: float(item.text())})
             elif col == 2:
-                self.session.query(Triangle).filter_by(id=triangle_id).update({Triangle.second_side: float(item.text())})
-            self.session.commit()
+                session.query(Triangle).filter_by(id=triangle_id).update({Triangle.second_side: float(item.text())})
+            session.commit()
+
+    def createTriangle(self):
+        session = Session()
+        first_side = self.ui.cntCatet1.value()
+        second_side = self.ui.cntCatet2.value()
+        new_triangle = Triangle(first_side, second_side)
+        session.add(new_triangle)
+        session.commit()
+
+        new_row_index = self.ui.tableWidget.rowCount()
+
+        self.ui.tableWidget.insertRow(new_row_index)
+        self.insertRow(new_triangle, new_row_index)
 
     def importData(self, item: QTableWidgetItem):
         row = item.row()
@@ -55,17 +58,14 @@ class DataBasePage(QMainWindow):
         self.parent().setEnabled(True)
         self.close()
 
-    def createTriangle(self):
-        first_side = self.ui.cntCatet1.value()
-        second_side = self.ui.cntCatet2.value()
-        new_triangle = Triangle(first_side, second_side)
-        self.session.add(new_triangle)
-        self.session.commit()
+    def insertRow(self, triangle: Triangle, row: int):
+        item_id = QTableWidgetItem(str(triangle.id))
+        item_first_side = QTableWidgetItem(str(triangle.first_side))
+        item_second_side = QTableWidgetItem(str(triangle.second_side))
 
-        new_row_index = self.ui.tableWidget.rowCount()
-
-        self.ui.tableWidget.insertRow(new_row_index)
-        self.insertRow(new_triangle, new_row_index)
+        self.ui.tableWidget.setItem(row, 0, item_id)
+        self.ui.tableWidget.setItem(row, 1, item_first_side)
+        self.ui.tableWidget.setItem(row, 2, item_second_side)
 
     def closeEvent(self, event: QCloseEvent):
         event.accept()
